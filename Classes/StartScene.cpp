a79f39b0e567024d741d70ae89fd5ba09f60c257
kozlo <kozlo@DESKTOP-JDKIKGN>
2017-02-03 21:44:13 +0300

    ui2

--- Classes/StartScene.cpp
+++ Classes/StartScene.cpp
@@ -28,7 +28,6 @@ bool StartScene::init()
                                            CC_CALLBACK_1(StartScene::menuChooseModeCallback, this));
 	chooseModeButton->setPosition(Vec2(visibleSize.width /2, 4 * visibleSize.height / 5));
 	
-
 	auto settingsButton = MenuItemImage::create(
 		"SettingsNormal.png",
 		"SettingsSelected.png",
